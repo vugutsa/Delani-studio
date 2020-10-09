cd2f346f8adae047dc3d687a897874d11e84c342
vugutsa <vugutsamercy84@gmail.com>
2020-10-09 22:14:08 +0300

    add background image of the services

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $("#one").click(function(){
         $("p").toggle();
     });
+
+   $("#two").click(function(){
+       $("p").toggle();
+
+   });
+   $("#three").click(function(){
+       $("p").toggle();
+   });
 });
